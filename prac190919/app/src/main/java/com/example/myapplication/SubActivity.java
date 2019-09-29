dda21503c81787cc5fc38c90b4d6e21e603e0a65
Jung <tlawjd99gma@gmail.com>
2019-09-29 21:30:15 +0900

    paint process implemented

--- prac190919/app/src/main/java/com/example/myapplication/SubActivity.java
+++ prac190919/app/src/main/java/com/example/myapplication/SubActivity.java
@@ -1,30 +1,15 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity{
+    MyView m;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.drawable);
 
-        Intent intent = new Intent(this.getIntent());
-        String s = intent.getStringExtra("text");
-        TextView textView = (TextView)findViewById(R.id.textview);
-        textView.setText(s);
+        m = new MyView(this);
+        setContentView(m);
 
-        Button btn = (Button)findViewById(R.id.mainBtn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mainIntent = new Intent(SubActivity.this, MainActivity.class);
-                startActivity(mainIntent);
-            }
-        });
     }
 }
